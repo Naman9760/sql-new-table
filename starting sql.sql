7a4774454d7c4791f79a074a8cb7b3c54330824a
Naman Diwakar <99721048+Naman9760@users.noreply.github.com>
2022-03-09 18:24:39 +0530

    Add files via upload

---
+++ starting sql.sql
@@ -0,0 +1,21 @@
+create database school
+use school
+create table Techers(eid int identity,name varchar(30),subject varchar(40),salary int,gender varchar(1))
+select * from Techers
+insert into Techers(name,subject,salary,gender)values
+('mukesh','chimstry',12000,'m'),
+('sursh','physic',14000,'m'),
+('rajesh','math',13000,'m'),
+('mukesh','hindi',16000,'m'),
+('rani','engish',19000,'f'),
+('pinky','histry',20000,'f')
+update Techers set salary=1670 where eid = 1
+select * from Techers where name = 'mukesh'
+update Techers set name='ashish' where eid=4 
+sp_rename 'Techers.eid',ID
+select * from Techers
+select sum(salary) from Techers 
+select salary from Techers where name='mukesh'
+select count(salary),count(gender) from Techers
+
+
